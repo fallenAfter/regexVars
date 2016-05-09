cddbb6231efe7fb5e461b112d0fcc125a6f0ed02
brandon e <legion.vall@gmail.com>
2016-05-09 15:21:47 -0400

    added basic email validator

--- regEx.js
+++ regEx.js
@@ -1,1 +1,6 @@
+//show that document is connected to page
 console.log('Connected RegEx');
+
+//email
+//variable to distinguishing an email
+var regEmail = new RegExp(/^[a-z0-9_.]{2,27}\@[a-z0-9]{2,27}\.[a-z0-9]{2,6}$/);
