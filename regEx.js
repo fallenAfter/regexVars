7adb59da470befc039af0c58d6d6d9fe5ba5d987
brandon e <legion.vall@gmail.com>
2016-05-09 18:22:02 -0400

    added postal code

--- regEx.js
+++ regEx.js
@@ -2,8 +2,8 @@
 console.log('Connected RegEx');
 
 //email
-var regEmail = new RegExp(/^(?![_.])+([a-z0-9_.]*([^._])){2,27}\@[a-z0-9]{2,27}\.[a-z0-9]{2,6}$/);
+var regEmail = new RegExp(/(?![_.])+([a-z0-9_.]*([^._])){2,27}\@[a-z0-9]{2,27}\.[a-z0-9]{2,6}/);
 //Phone
-var regPhone = new RegExp(/^(\+)?(\d{1,2})?\s?(\(?\d{3}\)|\d{3})?(\s|\.)?\d{3}(\s|\.|\-)?\d{4}$/);
+var regPhone = new RegExp(/(\+)?(\d{1,2})?\s?(\(?\d{3}\)|\d{3})?(\s|\.)?\d{3}(\s|\.|\-)?\d{4}$/);
 
-// +11 (222) 333-4444
+var regPostal = new RegExp(/([a-zA-Z]\d[a-zA-Z])\s?(\d[a-zA-Z]\d)/)
