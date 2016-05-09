1b8d109c39769cca5d57f0a7b0f54cbcee326b6b
brandon e <legion.vall@gmail.com>
2016-05-09 16:30:34 -0400

    added basic phone expression which only accepts full string w/o spaces ie 12223334444

--- regEx.js
+++ regEx.js
@@ -2,5 +2,6 @@
 console.log('Connected RegEx');
 
 //email
-//variable to distinguishing an email
 var regEmail = new RegExp(/^(?![_.])+([a-z0-9_.]*([^._])){2,27}\@[a-z0-9]{2,27}\.[a-z0-9]{2,6}$/);
+//Phone
+var regPhone = new RegExp(/^[0-3][0-9]{3}[0-9]{3}[0-9]{4}$/);
