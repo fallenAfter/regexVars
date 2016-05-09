f02700df556dbffcb9f1546c35e6a3c8a821ae30
brandon e <legion.vall@gmail.com>
2016-05-09 18:02:48 -0400

    adde multiple phone number input types

--- regEx.js
+++ regEx.js
@@ -4,4 +4,6 @@ console.log('Connected RegEx');
 //email
 var regEmail = new RegExp(/^(?![_.])+([a-z0-9_.]*([^._])){2,27}\@[a-z0-9]{2,27}\.[a-z0-9]{2,6}$/);
 //Phone
-var regPhone = new RegExp(/^[0-3][0-9]{3}[0-9]{3}[0-9]{4}$/);
+var regPhone = new RegExp(/^(\+)?(\d{1,2})?\s?(\(?\d{3}\)|\d{3})?(\s|\.)?\d{3}(\s|\.|\-)?\d{4}$/);
+
+// +11 (222) 333-4444
