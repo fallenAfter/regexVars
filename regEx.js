337171e47855109b07a9b5dc69488e863beceb78
brandon e <legion.vall@gmail.com>
2016-05-11 21:51:59 -0400

    added ipv4

--- regEx.js
+++ regEx.js
@@ -9,3 +9,5 @@ var regPhone = new RegExp(/^(\+)?(\d{1,2})?\s?(\(?\d{3}\)|\d{3})?(\s|\.)?\d{3}(\
 var regPostal = new RegExp(/^([a-zA-Z]\d[a-zA-Z])\s?(\d[a-zA-Z]\d)$/);
 //zip code
 var regZip = new RegExp(/^(\d{5})(\-\d{4})?$/);
+//ipv4 adress
+var regIpv4 = new RegExp(/^\d{3}\.\d{3}\.\d{1,2}\.\d{1,3}$/);
